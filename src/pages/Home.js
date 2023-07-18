e1a758b936f9b2923ab73a6b758619d834c723af
Vimal <vimalkumarak478@gmail.com>
2023-07-18 21:33:57 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Products from '../components/Products';
+
+const Home = () => {
+  return (
+    <div>
+      <>
+        <h2>Welcome to the PRIYA STORE</h2>
+        <section>
+          <Products/>
+        </section>
+      </>
+    </div>
+  )
+}
+
+export default Home;
